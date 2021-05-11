e53f67d8d3a0de4caef94978c2238ee9c42446ea
mapper895 <mapper.895.mp@gmail.com>
2021-05-11 10:32:27 -0500

    Login User Redirect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { auth, provider } from "../firebase";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,6 +14,15 @@ const Header = () => {
   const history = useHistory();
   const username = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        setUser(user);
+        history.push("/home");
+      }
+    });
+  }, [username]);
 
   const handleAuth = () => {
     auth
